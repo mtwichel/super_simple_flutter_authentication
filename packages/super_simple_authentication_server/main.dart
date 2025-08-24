e6c765851c2fcf55d23b51b13254a7c605802e30
Marcus Twichel <marc.twichy@gmail.com>
2025-08-24 15:31:04 -0600

    prep for release

--- packages/super_simple_authentication_server/main.dart
+++ packages/super_simple_authentication_server/main.dart
@@ -1,11 +1,7 @@
 import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
-import 'package:postgres_builder/postgres_builder.dart';
 import 'package:super_simple_authentication_toolkit/super_simple_authentication_toolkit.dart';
-
-import '../super_simple_authentication_hive_data_storage/lib/src/hive_data_storage.dart';
-import '../super_simple_authentication_postgres_data_storage/lib/src/postgres_data_storage.dart';
 
 late DataStorage dataStorage;
 
@@ -18,49 +14,49 @@ Future<void> init(InternetAddress ip, int port) async {
     dataStorage = InMemoryDataStorage();
   }
 
-  if (dataStorageType == 'hive') {
-    final storage = HiveDataStorage();
-    await storage.initialize(
-      databasePath: Platform.environment['HIVE_DATA_PATH'],
-    );
-    dataStorage = storage;
-  }
+  // if (dataStorageType == 'hive') {
+  //   final storage = HiveDataStorage();
+  //   await storage.initialize(
+  //     databasePath: Platform.environment['HIVE_DATA_PATH'],
+  //   );
+  //   dataStorage = storage;
+  // }
 
-  if (dataStorageType == 'postgres') {
-    final host = Platform.environment['POSTGRES_HOST'] ?? 'localhost';
-    final port = Platform.environment['POSTGRES_PORT'] ?? '5432';
-    final databaseName =
-        Platform.environment['POSTGRES_DATABASE'] ?? 'postgres';
-    final username = Platform.environment['POSTGRES_USERNAME'];
-    final password = Platform.environment['POSTGRES_PASSWORD'];
-    final endpoint = Endpoint(
-      host: host,
-      port: int.parse(port),
-      database: databaseName,
-      username: username,
-      password: password,
-    );
+  // if (dataStorageType == 'postgres') {
+  //   final host = Platform.environment['POSTGRES_HOST'] ?? 'localhost';
+  //   final port = Platform.environment['POSTGRES_PORT'] ?? '5432';
+  //   final databaseName =
+  //       Platform.environment['POSTGRES_DATABASE'] ?? 'postgres';
+  //   final username = Platform.environment['POSTGRES_USERNAME'];
+  //   final password = Platform.environment['POSTGRES_PASSWORD'];
+  //   final endpoint = Endpoint(
+  //     host: host,
+  //     port: int.parse(port),
+  //     database: databaseName,
+  //     username: username,
+  //     password: password,
+  //   );
 
-    final database = DirectPostgresBuilder(
-      debug: true,
-      logger: (sql) {
-        // ignore: avoid_print
-        print(sql);
-      },
-    );
+  //   final database = DirectPostgresBuilder(
+  //     debug: true,
+  //     logger: (sql) {
+  //       // ignore: avoid_print
+  //       print(sql);
+  //     },
+  //   );
 
-    await database.initialize(
-      endpoint: endpoint,
-      settings: const ConnectionSettings(sslMode: SslMode.disable),
-    );
+  //   await database.initialize(
+  //     endpoint: endpoint,
+  //     settings: const ConnectionSettings(sslMode: SslMode.disable),
+  //   );
 
-    final storage = PostgresDataStorage();
-    await storage.initialize(
-      endpoint: endpoint,
-      settings: const ConnectionSettings(sslMode: SslMode.disable),
-    );
-    dataStorage = storage;
-  }
+  //   final storage = PostgresDataStorage();
+  //   await storage.initialize(
+  //     endpoint: endpoint,
+  //     settings: const ConnectionSettings(sslMode: SslMode.disable),
+  //   );
+  //   dataStorage = storage;
+  // }
 }
 
 Future<HttpServer> run(Handler handler, InternetAddress ip, int port) {
